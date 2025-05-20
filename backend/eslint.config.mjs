e101c0c4ed4132936f866c21d098c44dda061f3e
Hitesh <hiteshsharma.2314@gmail.com>
2025-05-20 19:32:22 +0530

    solved minor setup issues

---
+++ backend/eslint.config.mjs
@@ -0,0 +1,38 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import prettierPlugin from 'eslint-plugin-prettier';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          semi: true,
+          singleQuote: true,
+          tabWidth: 2,
+          trailingComma: 'all',
+          printWidth: 100,
+          bracketSpacing: true,
+        },
+      ],
+    },
+  },
+]);
